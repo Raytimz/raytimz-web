b779d445e2ac917ceccfe01308f15d9001020b16
Raytimz <teplouhov.mitya@yandex.ru>
2026-08-26 07:54:51 +0200

    Revert "Add Russian landing page"
    This reverts commit f7fb8aec1ec0191d38f14be0a73058a9b50458ca.

--- app/ru/page.tsx
+++
@@ -1,6 +0,0 @@
-import LandingPage from '../landing-page';
-import { russianCopy } from '../translations';
-
-export default function RussianHome() {
-  return <LandingPage copy={russianCopy} />;
-}
